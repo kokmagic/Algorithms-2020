5388e9cf21caa04e9b3ba06ccccaaeac021c83bb
kok_magic <spikebrony@mail.ru>
2020-10-28 02:54:48 +0300

    2 урок!

--- src/lesson2/JavaAlgorithms.java
+++ src/lesson2/JavaAlgorithms.java
@@ -3,15 +3,17 @@ package lesson2;
 import kotlin.NotImplementedError;
 import kotlin.Pair;
 
+import java.util.Arrays;
+
 @SuppressWarnings("unused")
 public class JavaAlgorithms {
     /**
      * Получение наибольшей прибыли (она же -- поиск максимального подмассива)
      * Простая
-     *
+     * <p>
      * Во входном файле с именем inputName перечислены цены на акции компании в различные (возрастающие) моменты времени
      * (каждая цена идёт с новой строки). Цена -- это целое положительное число. Пример:
-     *
+     * <p>
      * 201
      * 196
      * 190
@@ -20,13 +22,13 @@ public class JavaAlgorithms {
      * 194
      * 193
      * 185
-     *
+     * <p>
      * Выбрать два момента времени, первый из них для покупки акций, а второй для продажи, с тем, чтобы разница
      * между ценой продажи и ценой покупки была максимально большой. Второй момент должен быть раньше первого.
      * Вернуть пару из двух моментов.
      * Каждый момент обозначается целым числом -- номер строки во входном файле, нумерация с единицы.
      * Например, для приведённого выше файла результат должен быть Pair(3, 4)
-     *
+     * <p>
      * В случае обнаружения неверного формата файла бросить любое исключение.
      */
     static public Pair<Integer, Integer> optimizeBuyAndSell(String inputName) {
@@ -36,49 +38,49 @@ public class JavaAlgorithms {
     /**
      * Задача Иосифа Флафия.
      * Простая
-     *
+     * <p>
      * Образовав круг, стоят menNumber человек, пронумерованных от 1 до menNumber.
-     *
+     * <p>
      * 1 2 3
      * 8   4
      * 7 6 5
-     *
+     * <p>
      * Мы считаем от 1 до choiceInterval (например, до 5), начиная с 1-го человека по кругу.
      * Человек, на котором остановился счёт, выбывает.
-     *
+     * <p>
      * 1 2 3
      * 8   4
      * 7 6 х
-     *
+     * <p>
      * Далее счёт продолжается со следующего человека, также от 1 до choiceInterval.
      * Выбывшие при счёте пропускаются, и человек, на котором остановился счёт, выбывает.
-     *
+     * <p>
      * 1 х 3
      * 8   4
      * 7 6 Х
-     *
+     * <p>
      * Процедура повторяется, пока не останется один человек. Требуется вернуть его номер (в данном случае 3).
-     *
+     * <p>
      * 1 Х 3
      * х   4
      * 7 6 Х
-     *
+     * <p>
      * 1 Х 3
      * Х   4
      * х 6 Х
-     *
+     * <p>
      * х Х 3
      * Х   4
      * Х 6 Х
-     *
+     * <p>
      * Х Х 3
      * Х   х
      * Х 6 Х
-     *
+     * <p>
      * Х Х 3
      * Х   Х
      * Х х Х
-     *
+     * <p>
      * Общий комментарий: решение из Википедии для этой задачи принимается,
      * но приветствуется попытка решить её самостоятельно.
      */
@@ -89,7 +91,7 @@ public class JavaAlgorithms {
     /**
      * Наибольшая общая подстрока.
      * Средняя
-     *
+     * <p>
      * Дано две строки, например ОБСЕРВАТОРИЯ и КОНСЕРВАТОРЫ.
      * Найти их самую длинную общую подстроку -- в примере это СЕРВАТОР.
      * Если общих подстрок нет, вернуть пустую строку.
@@ -97,21 +99,66 @@ public class JavaAlgorithms {
      * Если имеется несколько самых длинных общих подстрок одной длины,
      * вернуть ту из них, которая встречается раньше в строке first.
      */
-    static public String longestCommonSubstring(String firs, String second) {
-        throw new NotImplementedError();
+    static public String longestCommonSubstring(String first, String second) {
+        int[][] subLength = new int[first.length()][second.length()];
+        int maxLength = 0;
+        StringBuilder substring = new StringBuilder();
+        for (int i = 0; i < first.length(); i++) {
+            for (int j = 0; j < second.length(); j++) {
+                boolean equal = first.charAt(i) == second.charAt(j);
+                if ((i == 0 || j == 0) && equal) {
+                    subLength[i][j] = 1;
+                    break;
+                }
+                if (equal) {
+                    subLength[i][j] = subLength[i - 1][j - 1] + 1;
+                }
+                if (subLength[i][j] > maxLength) maxLength = subLength[i][j];
+            }
+        }
+        for (int i = 0; i < first.length(); i++) {
+            for (int j = 0; j < second.length(); j++) {
+               if (subLength[i][j] == maxLength) {
+                   int count = maxLength - 1;
+                   while (count >= 0) {
+                       substring.append(first.charAt(i - count));
+                       count--;
+                   }
+                   return substring.toString();
+               }
+            }
+        }
+        return "";
     }
 
     /**
      * Число простых чисел в интервале
      * Простая
-     *
+     * <p>
      * Рассчитать количество простых чисел в интервале от 1 до limit (включительно).
      * Если limit <= 1, вернуть результат 0.
-     *
+     * <p>
      * Справка: простым считается число, которое делится нацело только на 1 и на себя.
      * Единица простым числом не считается.
      */
     static public int calcPrimesNumber(int limit) {
-        throw new NotImplementedError();
+        //трудоемкость o(n log (log n)) ресурсоемкость o(n)
+        if (limit <= 1) return 0;
+        boolean[] primes = new boolean[limit + 1];
+        Arrays.fill(primes, true);
+        primes[0] = false;
+        primes[1] = false;
+        for (int i = 2; i * i <= limit; i++) {
+            if (primes[i]) {
+                for (int j = i * i; j <= limit; j += i) {
+                    primes[j] = false;
+                }
+            }
+        }
+        int count = 0;
+        for (boolean i : primes) {
+            if (i) count++;
+        }
+        return count;
     }
 }
